a4b33f5a9ab1c821dce5168a4fefab23a29d058c
Shaique Khan <esskay099@gmail.com>
2021-08-29 22:28:53 +0530

    Starting Point
    Set up package structure

---
+++ app/src/main/java/com/terranullius/clean_cache_mvvm/framework/presentation/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.terranullius.clean_cache_mvvm.framework.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.terranullius.clean_cache_mvvm.framework.presentation.composables.theme.CleanCache_MVVMTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
